9edc5861a5f3bd29fc8363b78dca78ffdfe930af
sarleone <sarah.m.leone@gmail.com>
2018-04-25 23:30:56 -0500

    added server and set up package.jsons

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
-// Add routes, both API and view
-app.use(routes);
+// // Serve up static assets
+// app.use(express.static("client/build"));
+// // Add routes, both API and view
+// app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/question-answer");
+// // Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/question-answer");
 
 // Start the API server
 app.listen(PORT, function() {
