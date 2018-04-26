2714e95214172749128154470e7f3f8c0d550474
sarleone <sarah.m.leone@gmail.com>
2018-04-25 21:18:05 -0500

    updated folders

---
+++ kis-react-app/client/src/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import Logo from "../../components/Logo";
+import Nav from "../../components/Nav";
+import Search from "../../components/Search";
+import UserQ from "../../components/UserQ";
+import UserR from "../../components/UserR";
+import Wrapper from "../../components/Wrapper";
+import API from "../../utils/API";
