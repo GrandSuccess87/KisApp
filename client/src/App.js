62a97123b860e43c8f1058f67de9bcb20aa0a435
Erica Barraca <ericabarraca@MacBook-Pro.local>
2018-04-26 22:48:34 -0500

    nav bar/logo components and css started

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import Logo from "./components/Logo";
+import Search from "./components/Search";
+
+
 
 const App = () => (
   <div>
     <Nav />
+    <Logo />
     <Home />
+
   </div>
 );
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./Nav.css";
 
 const Nav = props => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary navbar fixed-top">
-        <p className="nav-link-right"> KIS APP</p>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="index.html">KIS</a>
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar hamburger" ></span>
+        <span className="icon-bar hamburger"></span>
+        <span className="icon-bar hamburger"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="nav navbar-nav navbar-right">
+          <li className="nav-item active">
+            <a className="nav-link" href="index.html">#ASKQUESTIONS <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="about.html">ABOUT</a>
+            <li className="nav-item">
+              <a className="nav-link" href="resources.html">RESOURCES</a>
+            </li>
+          </li>
+        </ul>
+      </div>
     </nav>
 );
 
