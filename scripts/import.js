4cfeb9350970f9ed3144d796edf854e867f026dc
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-04-30 20:32:28 -0500

    pushing code to connect our server to mongodb, added algoliasearch library cdn to public/index.html

--- scripts/import.js
+++ scripts/import.js
@@ -1,32 +1,40 @@
 require("dotenv").config();
+const Db = require('mongodb').Db;
+Server = require('mongodb').Server;
+const keys = require('./keys.js');
+const algolia = require('algolia');
+const algoliasearch = require('algoliasearch');
 
-var keys = require('./keys.js');
-var algolia = require('algolia');
+var client = algoliasearch(keys.algoliaAppID, keys.alogoliaAdminKey);
+// our defined index name
+var index = client.initIndex('dev_questions');
 
-'use strict'
+//init connection to MongoDB
+var db = new Db('mydb', new Server('localhost', 27017));
+db.open(function(err, db){
+    //get the data in the collection
+    db.collection('myCollection', function(err, collection){
+        if(err) throw err;
+        console.log('Got the Collection Successfully' + collection)
 
-const algoliasearch = require('algoliasearch'),
-    client = algoliasearch('applicationID','apikey'),
-    // our defined index name
-    index = client.initIndex('dev_questions')
+        //iterate over the whole collectioin using a cursor
+        var batch = [];
+        collection.find().forEach(function(doc){
+            batch.push(doc);
+            if (batch.length > 1000) {
 
+                //send documents by batch of 1000 to Algolia
+                index.addObjects(batch);
+                batch = [];
+            }
+        });
 
-// Full language list on GitHub Gist - http://bit.ly/2kp5Mus
-let languages = ["Node.js, JavaScript"] 
+            // send the last batch
+            if (batch.length > 0) {
+                index.addObjects(batch);
+            }
 
-languages.forEach(function(language){
-    index.addObjects([{language: language}], function(err, indexed){
-        console.log(indexed)
-    })
-})
-
-
-// const algoliasearch = require('algoliasearch'),
-//       client        = algoliasearch('applicationID', 'apiKey'),
-//       index         = client.initIndex('programming-languages') 
-
-// languages.forEach(function(language) {
-//     index.addObjects([{ language: language }], function(err, indexed) {
-// 	console.log(indexed)
-//     })
-// })
+            console.log('Last Batch: ' + batch.length);
+            
+    });
+});
