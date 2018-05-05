d9164f78ec065309c42e0ffd4eca11b3caf3fe3d
Erica Barraca <ericabarraca@dhcp-10-105-235-177.wireless.northwestern.private>
2018-05-05 09:29:20 -0500

    trying to fix resources page

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./Nav.css";
+import {Link} from "react-router-dom";
 
 const Nav = props => (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">KIS</a>
+      <a className="navbar-brand" href="#">KIS</a>
 
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="navbar-right ">
-            <a class="nav-link" href="./about">ABOUT <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="navbar-right ">
+            <a className="nav-link" href="./about">ABOUT <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item navbar-right">
-            <a class="nav-link" href="resources">RESOURCES</a>
+          <li className="nav-item navbar-right">
+            <Link to="/resources" className="nav-link">RESOURCES</Link>
           </li>
         </ul>
 
