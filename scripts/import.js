ae8f64c9567f549f8af3354f5142c32d3c62cad5
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-05-07 19:24:11 -0500

    pushing code API Get Request Works on Back End

--- scripts/import.js
+++ scripts/import.js
@@ -9,21 +9,22 @@ var client = algoliasearch('YZR8H8JEYA', '8b1f7ac15d2dec622d1c39583ad47142');
 var index = client.initIndex('questions');
 var questionsJSON = require('./questions.json');
 
-router.get('api/questions', function(req, res){
-index.addObjects(questionsJSON, function(err, content) {
-  if (err) {
-    console.error(err);
-  }
-});
+router.get('/api/questions', function (req, res) {
+  index.addObjects(questionsJSON, function (err, content) {
+    if (err) {
+      console.error(err);
+    }
+  });
 
-// const searchQuestion = req.query.userQuestion;
+  // const searchQuestion = req.query.userQuestion;
 
-index.search(req.query.userQuestion, function(err, content, res) {
-    console.log(content.hits);
+  index.search(req.query.userQuestion, function (err, content) {
+    if (err) throw err;
+    // console.log(content.hits);
     res.json(content.hits);
-});
+    //
+  });
 
 });
 
 module.exports = router;
-
