cf9b90aec76aabd0c2ccf9a699c66ca8d20905f7
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-05-09 10:10:14 -0500

    pushing code for google authentication

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About"
 import ResourcesPage from "./pages/Resources"
+import GoogleLogin from "react-google-login"
 
 
 
-
+const responseGoogle = (response) => {
+  console.log(response);
+}
+ 
 const App = () => (
   <Router>
   <div>
+  <GoogleLogin
+    clientId="73580094877-njrsmo353tvtk1kt4c36mqgfiqumbll5.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+  />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
