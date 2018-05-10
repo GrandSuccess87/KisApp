1fafd30c3525498c1ba59259aa0250ded749e1c5
Ozzy Cardenas <ocardenas789@me.com>
2018-05-09 20:59:09 -0500

    result cards funcitonal, needs styling

--- client/src/components/UserResults/UserResults.js
+++ client/src/components/UserResults/UserResults.js
@@ -1,60 +1,72 @@
 import React from "react";
 import "./UserResults.css";
 
-const UserResults = props => (    
-  <div className = "container">
-  <div className="row justify-content-lg-center">
-    <div className="col-lg-12" id="contentDiv">
-      <div className="accordion" id="accordion">
-        
-        <div className="card">
-          <div className="card-header" id="headingOne">
-            <h5 className="mb-0">
-              <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                Collapsible Group Item #1
-              </button>
-            </h5>
-          </div>
-          <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-            <div className="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.                  </div>
-          </div>
-        </div>
-        
-        <div className="card">
-          <div className="card-header" id="headingTwo">
-            <h5 className="mb-0">
-              <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Collapsible Group Item #2
-              </button>
-            </h5>
-          </div>
-          <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div className="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
-            </div>
-          </div>
-        </div>
 
-        <div className="card">
-          <div className="card-header" id="headingThree">
-            <h5 className="mb-0">
-              <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Collapsible Group Item #3
-              </button>
-            </h5>
-          </div>
-          <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-            <div className="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+
+const UserResults = props => { 
+  console.log(props);
+
+  if(props.results.length > 0){
+    return (    
+      <div className = "container">
+      <div className="row justify-content-lg-center">
+        <div className="col-lg-12" id="contentDiv">
+          <div className="accordion" id="accordion">
+            
+            <div className="card">
+              <div className="card-header" id="headingOne">
+                <h5 className="mb-0">
+                  <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    {props.results[0].question}
+                  </button>
+                </h5>
+              </div>
+              <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div className="card-body">
+                {props.results[0].answer}
+                </div>
+              </div>
             </div>
+            
+            <div className="card">
+              <div className="card-header" id="headingTwo">
+                <h5 className="mb-0">
+                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  {props.results[1].question}
+                  </button>
+                </h5>
+              </div>
+              <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div className="card-body">
+                {props.results[1].answer}
+                </div>
+              </div>
+            </div>
+    
+            <div className="card">
+              <div className="card-header" id="headingThree">
+                <h5 className="mb-0">
+                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  {props.results[2].question}
+                  </button>
+                </h5>
+              </div>
+              <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div className="card-body">
+                {props.results[2].answer}
+                </div>
+              </div>
+            </div>
+    
           </div>
         </div>
-
       </div>
-    </div>
-  </div>
-  </div>
-)
+      </div>
+    )} else {
+      return null
+    }
+  } 
+
+  
 
 export default UserResults;
