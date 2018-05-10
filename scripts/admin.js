d79c4f66cae2b336f29cc8dfa22a4f592f56af5f
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-05-10 06:42:43 -0500

    pushing code for admin

--- scripts/admin.js
+++ scripts/admin.js
@@ -3,12 +3,17 @@ const db = require("../models");
 const router = require('express').Router();
 
 router.get('/api/admin/questions', function (req, res) {
-    index.showObjects(adminQuestions, function (err, content) {
+    // create a function that allows the user to login using react google login
+
+    // search the algolia database for unanswered questions and present them to the admin
+    index.search(adminQuestion, function (err, content) {
       if (err) {
         console.error(err);
       }
-      res.json(content)      
+      res.json(content.hits.question)      
     });
 });
+
+// create a function so that the admin id matches the object id that was answered
 
 module.exports = router;
