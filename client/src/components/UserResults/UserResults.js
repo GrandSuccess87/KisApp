ba29f7c7120381c70c15a86731228b22db6b8e94
Ozzy Cardenas <ocardenas789@me.com>
2018-05-11 16:02:23 -0500

    userResults bugs

--- client/src/components/UserResults/UserResults.js
+++ client/src/components/UserResults/UserResults.js
@@ -16,13 +16,17 @@ const UserResults = props => {
             <div className="card">
               <div className="card-header" id="headingOne">
                 <h5 className="mb-0">
-                  <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  <button 
+                  id="button-text"
+                  className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                     {props.results[0].question}
                   </button>
                 </h5>
               </div>
               <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                <div className="card-body">
+                <div 
+                id="card-body-styling"
+                className="card-body">
                 {props.results[0].answer}
                 </div>
               </div>
@@ -31,13 +35,17 @@ const UserResults = props => {
             <div className="card">
               <div className="card-header" id="headingTwo">
                 <h5 className="mb-0">
-                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  <button 
+                  id="button-text"
+                  className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                   {props.results[1].question}
                   </button>
                 </h5>
               </div>
               <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div className="card-body">
+                <div 
+                id="card-body-styling"
+                className="card-body">
                 {props.results[1].answer}
                 </div>
               </div>
@@ -46,13 +54,17 @@ const UserResults = props => {
             <div className="card">
               <div className="card-header" id="headingThree">
                 <h5 className="mb-0">
-                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  <button 
+                  id="button-text"
+                  className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                   {props.results[2].question}
                   </button>
                 </h5>
               </div>
               <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                <div className="card-body">
+                <div 
+                id="card-body-styling"
+                className="card-body">
                 {props.results[2].answer}
                 </div>
               </div>
