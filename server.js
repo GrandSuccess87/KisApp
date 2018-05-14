579767b9f1bd3b9dd81d40a0f6d28b5db92b4954
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-05-14 18:27:10 -0500

    fixing routing error

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // Serve up static assets
-app.use(express.static("client/build"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.use(express.static("client/build"));
 // Add routes, both API and view
 // app.use(routes);
 
 // API Routes
 app.use("/", require('./scripts/import.js'));
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/KisAppDatabase");
