248772fc9ae5da94366c1878a627a93da515b94a
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-05-15 12:07:38 -0500

    fixed merge conflict in googleSingOut.js

--- client/src/GoogleLogout.js
+++ client/src/GoogleLogout.js
@@ -60,10 +60,7 @@ class GoogleLogout extends Component {
       fontSize: 16,
       fontWeight: 'bold',
       fontFamily: 'Roboto',
-<<<<<<< HEAD
-=======
       marginLeft: 20,
->>>>>>> master
     }
     const styleProp = (() => {
       if (style) {
